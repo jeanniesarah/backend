826b2eb5449d0be48d86fb2980ad3b8e9e238a02
Kirill Tereshchenko <elpuffru@gmail.com>
2020-02-11 16:54:26 +0300

    (add) segment analytic events for look at survey statistic

--- services/stat.service.js
+++ services/stat.service.js
@@ -3,6 +3,8 @@
 const DbService = require("moleculer-db");
 const MongoDBAdapter = require("moleculer-db-adapter-mongo");
 const uuid = require("uuid");
+const Analytics = require("analytics-node");
+const analytics = new Analytics(process.env.SEGMENT_WRITE_KEY);
 
 module.exports = {
 	name: "stat",
@@ -11,106 +13,126 @@ module.exports = {
 	collection: "stats",
 
 	/**
-     * Service settings
-     */
+	 * Service settings
+	 */
 	settings: {},
 
 	/**
-     * Service dependencies
-     */
+	 * Service dependencies
+	 */
 	dependencies: [],
 
 	/**
-     * Actions
-     */
+	 * Actions
+	 */
 	actions: {
 		table: {
 			async handler(ctx) {
 				const {survey_id} = ctx.params;
-        await ctx.call("survey.checkSurveyAccess", {survey_id});
-        const questions = await ctx.call('question.getBySurveyId', {survey_id})
+				await ctx.call("survey.checkSurveyAccess", {survey_id});
+				const questions = await ctx.call("question.getBySurveyId", {survey_id});
 				const completedSurveys = await ctx.call("completedSurvey.getForSurvey", {survey_id});
-        const results = []
-        console.log({completedSurveys})
-        for (let completedSurvey of completedSurveys) {
-          console.log({completedSurvey})
-          const answers = await ctx.call("answers.getForCompletedSurvey", {survey_id: completedSurvey._id});
-          results.push({...completedSurvey, ...{answers}})
-        }
-        console.log(results)
-        for (let result of results) {
-          const answers = questions.map(question => {
-            const answ = result.answers.find((answer) => String(answer.questionId) === String(question._id))
-            if (answ){
-              console.log(answ)
-              return answ.value
-            } else {
-              return null
-            } 
-          }) 
-          result.answers = answers
-        }
-       
+				const results = [];
+				console.log({completedSurveys});
+				for (let completedSurvey of completedSurveys) {
+					console.log({completedSurvey});
+					const answers = await ctx.call("answers.getForCompletedSurvey", {survey_id: completedSurvey._id});
+					results.push({...completedSurvey, ...{answers}});
+				}
+				console.log(results);
+				for (let result of results) {
+					const answers = questions.map(question => {
+						const answ = result.answers.find((answer) => String(answer.questionId) === String(question._id));
+						if (answ) {
+							console.log(answ);
+							return answ.value;
+						} else {
+							return null;
+						}
+					});
+					result.answers = answers;
+				}
+
+				this.setAnalytic({user: ctx.meta.user}, "Looked at statistic", survey_id);
+
 				return {
-          questions,
-          results,
-          //answers
-        };
+					questions,
+					results,
+					//answers
+				};
 			}
 		},
 		piechart: {
 			async handler(ctx) {
 				const {survey_id} = ctx.params;
 				await ctx.call("survey.checkSurveyAccess", {survey_id});
-        const questions = await ctx.call('question.getBySurveyId', {survey_id})
-        const chart = []
-        for (let question of questions) {
-          const yesCount = await ctx.call('answers.count', {
-            query: {
-              questionId: question._id,
-              value: true
-            }
-          })
-          chart.push({
-            _id: question._id,
-            text: question.text,
-            yesCount
-          })
-        }
-        return chart
+				const questions = await ctx.call("question.getBySurveyId", {survey_id});
+				const chart = [];
+				for (let question of questions) {
+					const yesCount = await ctx.call("answers.count", {
+						query: {
+							questionId: question._id,
+							value: true
+						}
+					});
+					chart.push({
+						_id: question._id,
+						text: question.text,
+						yesCount
+					});
+				}
+				return chart;
 			}
-    }
-	
+		}
+
 
 	},
 
 	/**
-     * Events
-     */
+	 * Events
+	 */
 	events: {},
 
 	/**
-     * Methods
-     */
-	methods: {},
+	 * Methods
+	 */
+	methods: {
+		setAnalytic({user}, event, survey_id) {
+			const uid = user._id.toString();
+			analytics.identify({
+				userId: uid,
+				traits: {
+					email: user.email
+				},
+			});
+			analytics.track({
+				userId: uid,
+				event,
+				properties: {
+					survey_id
+				},
+				timestamp: new Date(),
+			});
+		}
+	},
 
 	/**
-     * Service created lifecycle event handler
-     */
+	 * Service created lifecycle event handler
+	 */
 	created() {
 
 	},
 
 	/**
-     * Service started lifecycle event handler
-     */
+	 * Service started lifecycle event handler
+	 */
 	started() {
 
 	},
 
 	/**
-     * Service stopped lifecycle event handler
-     */
+	 * Service stopped lifecycle event handler
+	 */
 	stopped() {
 
 	}
